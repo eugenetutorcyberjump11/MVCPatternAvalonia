077e5c2559c26f10570b5bfad4878f4518e1e22d
demenko <eugenetutor@gmail.com>
2024-07-10 14:42:54 +0300

    Add project files.

---
+++ MVCPatternAvalonia/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace MVCPatternAvalonia.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
